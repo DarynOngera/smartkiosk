5d1fb81900d8a1b2ac18ba98acea2d895d0e0eef
Ongera <ongeradaryn@gmail.com>
2026-05-08 10:55:17 +0300

    chore: implement customer-user relationship

--- apps/smart_kiosk_core/lib/smart_kiosk_core/schemas/customer.ex
+++ apps/smart_kiosk_core/lib/smart_kiosk_core/schemas/customer.ex
@@ -16,13 +16,14 @@ defmodule SmartKioskCore.Schemas.Customer do
     field :notes, :string
 
     belongs_to :shop,   SmartKioskCore.Schemas.Shop
+    belongs_to :user,   SmartKioskCore.Schemas.User
     has_many   :orders, SmartKioskCore.Schemas.Order
 
     timestamps(type: :utc_datetime)
   end
 
   @required ~w(shop_id)a
-  @optional ~w(name phone email notes)a
+  @optional ~w(name phone email notes user_id)a
 
   def changeset(customer, attrs) do
     customer
@@ -32,6 +33,7 @@ defmodule SmartKioskCore.Schemas.Customer do
     |> validate_format(:phone, ~r/^\+?[\d\s\-]{9,15}$/)
     |> unique_constraint([:shop_id, :phone], message: "phone already registered for this shop")
     |> foreign_key_constraint(:shop_id)
+    |> foreign_key_constraint(:user_id)
   end
 end
 
